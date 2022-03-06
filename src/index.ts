3af58477b6033a5283f6c29dbe420abfcc0fdd26
kaname-png <inmortaldragonxspace@outlook.com>
2022-03-06 11:54:53 -0600

    feat(core): export Command and Listener structure

--- src/index.ts
+++ src/index.ts
@@ -4,3 +4,6 @@ export * from './utils/enums/events';
 export * from './utils/enums/command';
 export * from './utils/interfaces/client';
 export * from './utils/interfaces/command';
+
+export * from './lib/structures/command';
+export * from './lib/structures/listener';
