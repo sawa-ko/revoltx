d637ee6bf976289683eaeb00856f4413a7120b54
kaname-png <inmortaldragonxspace@outlook.com>
2022-03-06 11:11:02 -0600

    feat(types): exported command enums

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 export * from './lib/client';
 
 export * from './utils/enums/events';
+export * from './utils/enums/command';
 export * from './utils/interfaces/client';
 export * from './utils/interfaces/command';
