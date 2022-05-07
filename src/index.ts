2612d55a3d655155268dcff949e409ee3c59b3e6
kaname-png <inmortaldragonxspace@outlook.com>
2022-05-07 17:51:32 -0600

    feat: export more types, utils and classes

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,3 @@
-export * from './lib/client';
-export * from './lib/parsers/args';
 export * from './lib/preconditions/conditions';
 export * from './lib/preconditions/precondition-container';
 export * from './lib/preconditions/precondition-container-array';
@@ -7,12 +5,14 @@ export * from './lib/preconditions/precondition-container-single';
 
 export * from './utils/enums/events';
 export * from './utils/enums/command';
+export * from './utils/enums/listener';
+
 export * from './utils/interfaces/client';
 export * from './utils/interfaces/command';
 export * from './utils/interfaces/argument';
 export * from './utils/interfaces/precondition';
-
-export * from './lib/utils/permissions';
+export * from './utils/interfaces/argument';
+export * from './utils/interfaces/channel';
 
 export * from './lib/structures/command';
 export * from './lib/structures/listener';
@@ -36,6 +36,25 @@ export * from './lib/resolvers/number';
 export * from './lib/resolvers/saved-messages-channel';
 export * from './lib/resolvers/string';
 export * from './lib/resolvers/user';
+
+export * from './lib/errors/argument-error';
+export * from './lib/errors/identifiers';
+export * from './lib/errors/precondition-error';
+export * from './lib/errors/user-error';
+
+export * from './lib/parsers/args';
+export * from './lib/parsers/maybe';
+export * from './lib/parsers/result';
+
+export * from './lib/structures/stores/argument';
+export * from './lib/structures/stores/command';
+export * from './lib/structures/stores/listener';
+export * from './lib/structures/stores/precondition';
+
+export * from './lib/client';
+export * from './lib/utils/permissions';
+export * from './utils/strategies/flag-unordered-strategy';
+export * from './utils/regex';
 
 export { container } from '@sapphire/pieces';
 export { API as Revolt } from 'revolt.js';
